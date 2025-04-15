b285052b5fe08d3237abdd0aab3289b15c7a8c57
Jesmin Chakma <jesminchakma39@gmail.com>
2025-04-15 10:58:18 +0600

    Banner Complete with Carousel Slider

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../components/Banner';
 
 const Home = () => {
     return (
-        <div className='sm:max-w-7xl mx-auto px-2 mt-10'> 
-            <h2>This is home</h2>
+        <div className='sm:max-w-7xl mx-auto px-2 mt-5'> 
+            <Banner/>
         </div>
     );
 };
---
+++ src/components/Banner.jsx
@@ -0,0 +1,47 @@
+// Banner.jsx
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+import img1 from '../assets/images/home/01.jpg';
+import img2 from '../assets/images/home/02.jpg';
+import img3 from '../assets/images/home/03.png';
+import img4 from '../assets/images/home/04.jpg';
+import img5 from '../assets/images/home/05.png';
+import img6 from '../assets/images/home/06.png';
+
+const Banner = () => {
+    const sliderArray = [img1, img2, img3, img4, img5, img6];
+    return (
+        <div className="w-full max-w-7xl mx-auto mt-5">
+            <Carousel
+                autoPlay
+                interval={1000}
+                infiniteLoop
+                showThumbs={false}
+                thumbWidth={100}
+                renderThumbs={() =>
+                    sliderArray.map((src, index) => (
+                        <img
+                            key={index}
+                            src={src}
+                            alt='Thumbnail'
+                            className='object-cover'
+                        />
+
+                    ))
+
+                }
+            >
+                {sliderArray.map((src, index) => (
+                    <div key={index}>
+                        <img src={src} alt='Slider-Image' className='h-130' />
+
+                    </div>
+                ))}
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
